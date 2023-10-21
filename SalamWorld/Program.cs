cc7f5ce3ad54a59654574d2138a6cbbfff3a6e79
Muhammad Osaid Faruqi <mosaidfaruqi@outlook.com>
2023-10-21 23:53:53 +0500

    Practice User Input And If Else Condition.

--- SalamWorld/Program.cs
+++ SalamWorld/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Salam, World!");
+Console.WriteLine();
+
+Console.Write("What is your First Name: ");
+string? firstName = Console.ReadLine();
+
+Console.WriteLine("Please, Allow me to ask your Last Name. :) (y/n)");
+string? allow = Console.ReadLine();
+
+if(allow == "y")
+{
+    Console.Write("What is your Last Name: ");
+    string? lastName = Console.ReadLine();
+    Console.WriteLine("So, Your Full Name is {0} {1}.", firstName, lastName);
+}
+else
+{
+    Console.WriteLine("Your First Name is {0}.", firstName);
+}
+
+Console.ReadLine();
